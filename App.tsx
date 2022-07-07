7ec68948ae726a130b87483a51faa249d5f5bf56
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2022-07-06 21:33:57 -0300

    adicionando navegacao

--- App.tsx
+++ App.tsx
@@ -9,6 +9,8 @@ import {
   Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
 import theme from "./src/global/styles/theme";
+import { NavigationContainer } from "@react-navigation/native";
+import { AppRoutes } from "./src/routes/app.routes";
 import { StatusBar } from "expo-status-bar";
 import { Dashboard } from "./src/screens/Dashboard";
 import { Register } from "./src/screens/Register";
@@ -63,7 +65,9 @@ export default function App() {
             flex: 1,
           }}
         >
-          <Register />
+          <NavigationContainer>
+            <AppRoutes />
+          </NavigationContainer>
         </View>
       </ThemeProvider>
     </>
